8ee9f7e0de3f5a2969eee08f612eccc2fdecd5ce
FunThomas424242 <funthomas424242@gmail.com>
2016-06-11 23:51:32 +0200

    Test parametrisiert
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- feature-toggle-example/src/test/java/com/github/funthomas424242/feature/toggle/example/command/MessageCommandTest.java
+++ feature-toggle-example/src/test/java/com/github/funthomas424242/feature/toggle/example/command/MessageCommandTest.java
@@ -1,28 +1,63 @@
 package com.github.funthomas424242.feature.toggle.example.command;
 
-import static com.github.funthomas424242.feature.toggle.example.config.Features.FEATURE_HELLO;
 import static com.github.funthomas424242.feature.toggle.example.config.Features.FEATURE_HALLO;
+import static com.github.funthomas424242.feature.toggle.example.config.Features.FEATURE_HELLO;
 import static com.github.funthomas424242.feature.toggle.example.config.Features.FEATURE_HERO;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
+@RunWith(Parameterized.class)
 public class MessageCommandTest {
 
 	MessageCommand command;
 
+	@Parameters
+	public static Collection<Boolean[]> data() {
+		final Boolean[][] data = new Boolean[][]{{false, false, false},
+				{false, false, true}, {false, true, false,}};
+		return Arrays.asList(data);
+	}
+
+	public MessageCommandTest(final boolean featureHelloState,
+			final boolean featureHalloState, final boolean featureHeroState) {
+
+		if (featureHelloState) {
+			System.setProperty("FEATURE_HELLO", "true");
+		} else {
+			System.clearProperty("FEATURE_HELLO");
+		}
+		if (featureHalloState) {
+			System.setProperty("FEATURE_HALLO", "true");
+		} else {
+			System.clearProperty("FEATURE_HALLO");
+		}
+		if (featureHeroState) {
+			System.setProperty("FEATURE_HERO", "true");
+		} else {
+			System.clearProperty("FEATURE_HERO");
+		}
+		command = new MessageCommand();
+	}
+
 	@BeforeClass
 	public static void initClass() {
-		// System.setProperty("FEATURE_HALLO", "true");
+
 	}
 
 	@Before
 	public void setUp() {
-		command = new MessageCommand();
+
 	}
 
 	@Test
