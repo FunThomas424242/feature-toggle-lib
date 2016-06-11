705b5fc011967e7ae48ac284ee1b83f0513c8a1b
FunThomas424242 <funthomas424242@gmail.com>
2016-06-11 23:50:39 +0200

    FEATURE_HERO implementiert
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- feature-toggle-example/src/main/java/com/github/funthomas424242/feature/toggle/example/command/MessageCommand.java
+++ feature-toggle-example/src/main/java/com/github/funthomas424242/feature/toggle/example/command/MessageCommand.java
@@ -2,6 +2,7 @@ package com.github.funthomas424242.feature.toggle.example.command;
 
 import static com.github.funthomas424242.feature.toggle.example.config.Features.FEATURE_HALLO;
 import static com.github.funthomas424242.feature.toggle.example.config.Features.FEATURE_HELLO;
+import static com.github.funthomas424242.feature.toggle.example.config.Features.FEATURE_HERO;
 
 import org.springframework.stereotype.Component;
 
@@ -18,6 +19,10 @@ public class MessageCommand {
 		}
 		if (FEATURE_HELLO.isActive()) {
 			message.append("Hello");
+			keinFeatureAktiv = false;
+		}
+		if (FEATURE_HERO.isActive()) {
+			message.append("HeRo");
 			keinFeatureAktiv = false;
 		}
 		if (keinFeatureAktiv) {
