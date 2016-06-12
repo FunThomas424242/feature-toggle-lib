a6adeb37ced3df8eed0444ee2c7e3c9ac1b96dc7
FunThomas424242 <funthomas424242@gmail.com>
2016-06-12 21:21:56 +0200

    Logpunkte erweitert
    Signed-off-by: FunThomas424242 <funthomas424242@gmail.com>

--- feature-toggle-example/src/main/java/com/github/funthomas424242/feature/toggle/example/command/MessageCommand.java
+++ feature-toggle-example/src/main/java/com/github/funthomas424242/feature/toggle/example/command/MessageCommand.java
@@ -4,12 +4,17 @@ import static com.github.funthomas424242.feature.toggle.example.config.Features.
 import static com.github.funthomas424242.feature.toggle.example.config.Features.FEATURE_HELLO;
 import static com.github.funthomas424242.feature.toggle.example.config.Features.FEATURE_HERO;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MessageCommand {
 
+	static final Logger LOG = LoggerFactory.getLogger(MessageCommand.class);
+
 	public String getMessage() {
+		LOG.debug("Command ThreadId:" + Thread.currentThread().getId());
 		final StringBuffer message = new StringBuffer();
 		message.append("Aktive Features: ");
 		boolean keinFeatureAktiv = true;
--- feature-toggle-lib/src/main/java/com/github/funthomas424242/libs/toggle/FeatureProvider.java
+++ feature-toggle-lib/src/main/java/com/github/funthomas424242/libs/toggle/FeatureProvider.java
@@ -3,45 +3,64 @@ package com.github.funthomas424242.libs.toggle;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FeatureProvider {
 
-	protected Map<Thread, AbstractFeatureManager> mapThreadToManager = new HashMap<>();
+	static final Logger LOG = LoggerFactory.getLogger(FeatureProvider.class);
+
+	protected Map<Long, AbstractFeatureManager> mapThreadIdToManager = new HashMap<>();
 
 	protected FeatureProvider() {
 		final Thread thread = Thread.currentThread();
 		final AbstractFeatureManager featureManager = new FeatureManager();
-		mapThreadToManager.put(thread, featureManager);
+		mapThreadIdToManager.put(thread.getId(), featureManager);
 	}
 
 	protected AbstractFeatureManager getFeatureManager() {
 		final Thread thread = Thread.currentThread();
-		return mapThreadToManager.get(thread);
+		return mapThreadIdToManager.get(thread.getId());
 	}
 
 	protected ModifiableFeatureManager getModifiableFeatureManager() {
 		final Thread thread = Thread.currentThread();
-		final AbstractFeatureManager oldManager = mapThreadToManager
-				.get(thread);
+		final AbstractFeatureManager oldManager = mapThreadIdToManager
+				.get(thread.getId());
 		final boolean isModifiable = oldManager.isModifiable();
 		if (isModifiable) {
 			return (ModifiableFeatureManager) oldManager;
 		} else {
 			final ModifiableFeatureManager newManager = new ModifiableFeatureManager(
 					oldManager);
-			mapThreadToManager.put(thread, newManager);
+			mapThreadIdToManager.put(thread.getId(), newManager);
 			return newManager;
 		}
 	}
 
 	public void registerClass(final FeatureToggle featureTogglesClass,
 			final String featureToggleName) {
+		LOG.debug(
+				"Provider Register ThreadId:" + Thread.currentThread().getId());
 		final AbstractFeatureManager featureManager = getFeatureManager();
 		featureManager.registerClass(featureTogglesClass, featureToggleName);
 	}
 
 	public boolean isActive(final FeatureToggle featureToggle) {
+		LOG.debug(
+				"Provider isActive ThreadId:" + Thread.currentThread().getId());
+
 		final AbstractFeatureManager featureManager = getFeatureManager();
+		LOG.debug("Provider isActive FeatureManager:" + featureManager);
 		return featureManager.isActive(featureToggle);
+		// if (featureManager == null) {
+		// LOG.debug("Provider isActive NULL in ThreadId:"
+		// + Thread.currentThread().getId());
+		// } else {
+		//
+		// }
+		//
+		// return false;
 	}
 
 }
